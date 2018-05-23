430c09861e7a93ec1280b42d3c44a859e3f1c430
Pietro Martinelli <amicofragile@gmail.com>
2018-05-23 09:24:16 +0200

    Replace multiple return with 'result' variable

--- src/main/java/it/unibs/ing/se/refactoring/fizzbuzz/FizzBuzz.java
+++ src/main/java/it/unibs/ing/se/refactoring/fizzbuzz/FizzBuzz.java
@@ -2,16 +2,22 @@ package it.unibs.ing.se.refactoring.fizzbuzz;
 
 public class FizzBuzz {
 	public static String play(int input) {
-		if(isMultipleOf(input, 15)) {
-			return "fizzbuzz";
+		String result = null;
+		if (isMultipleOf(input, 15)) {
+			result = "fizzbuzz";
+		} else if (isMultipleOf(input, 3)) {
+			result = "fizz";
+		} else if (isMultipleOf(input, 5)) {
+			result = "buzz";
 		}
-		if(isMultipleOf(input, 3)) {
-			return "fizz";
+		if (hasNoValue(result)) {
+			result = Integer.toString(input);
 		}
-		if(isMultipleOf(input, 5)) {
-			return "buzz";
-		}
-		return Integer.toString(input);
+		return result;
+	}
+
+	private static boolean hasNoValue(String result) {
+		return result == null;
 	}
 
 	private static boolean isMultipleOf(int input, int divider) {
