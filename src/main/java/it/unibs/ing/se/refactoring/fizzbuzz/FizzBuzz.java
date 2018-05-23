69c4a8d697b7a9b3384f2d46ae3eafaa787e316e
Pietro Martinelli <amicofragile@gmail.com>
2018-05-23 10:37:08 +0200

    Use foreach and StringBuilder

--- src/main/java/it/unibs/ing/se/refactoring/fizzbuzz/FizzBuzz.java
+++ src/main/java/it/unibs/ing/se/refactoring/fizzbuzz/FizzBuzz.java
@@ -1,36 +1,29 @@
 package it.unibs.ing.se.refactoring.fizzbuzz;
 
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.TreeMap;
 
 public class FizzBuzz {
-	private static final String Empty = "";
-	
 	private static final Map<Integer, String> cases = new TreeMap<Integer, String>();
-	
+
 	static {
 		cases.put(3, "fizz");
 		cases.put(5, "buzz");
 		cases.put(7, "bang");
 	}
-	
-	public static String play(int input) {
-		String result = cases.entrySet().stream().reduce(Empty, (acc, curr) -> isMultipleOf(input, curr.getKey()) ? combine(acc, curr.getValue()) : acc, FizzBuzz::combine);		
-		if (hasNoValue(result)) {
-			result = Integer.toString(input);
-		}
-		return result;
-	}
 
-	private static boolean hasNoValue(String result) {
-		return Empty.equals(result);
+	public static String play(int input) {
+		StringBuilder buffer = new StringBuilder();
+		for (Entry<Integer, String> c : cases.entrySet()) {
+			if (isMultipleOf(input, c.getKey())) {
+				buffer.append(c.getValue());
+			}
+		}
+		return buffer.length() == 0 ? Integer.toString(input) : buffer.toString();
 	}
 
 	private static boolean isMultipleOf(int input, int divider) {
 		return input % divider == 0;
-	}
-	
-	private static String combine(String x, String y) {
-		return x + y;
 	}
 }
